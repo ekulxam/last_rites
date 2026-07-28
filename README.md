7a23cad27b8da610c6de67a5623611821545c2a6
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-07-27 23:57:08 -0700

    Kotlin datagen

---
+++ README.md
@@ -0,0 +1,7 @@
+# Last Rites
+
+An alternative graves mod
+
+## Credits
+Eyalego - Idea and Assets
+SkyNotTheLimit - Code
---
+++ src/main/java/survivalblock/last_rites/LastRites.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2026-present ekulxam
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package survivalblock.last_rites;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LastRites implements ModInitializer {
+	public static final String MOD_ID = "last_rites";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
